1f456f4d9d5ed4edd2f2f23f02997d6eec7e3c84
Yuan Li <yuan.li@servian.com>
2023-04-12 06:54:21 +0000

    applied macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -12,7 +12,8 @@ select
     paymentmethod as payment_method,
     -- amount is stored in cents, convert it to dollarsß
     status as payment_status,
-    amount / 100 as amount,
+    -- amount / 100 as amount,
+    {{ cents_to_dollars('amount', 4) }} as amount,
     created as created_at
 
 from source
