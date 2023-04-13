455ac5c76c4d78567030408c557ac5916d3e8bda
Yuan Li <yuan.li@servian.com>
2023-04-13 05:19:16 +0000

    refactoring code by following CTE grouping and best practise

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,44 +1,34 @@
-with customers as (
+with
+    customers as (select * from {{ ref("stg_customers") }}),
 
-    select * from {{ ref('stg_customers')}}
+    orders as (select * from {{ ref("stg_orders") }}),
 
-),
+    payments as (select * from {{ ref("stg_payments") }}),
 
-orders as (
+    order_payments as (
 
-    select * from {{ ref('stg_orders') }}
+        select
+            order_id,
+            sum(case when payment_status = 'success' then amount end) as amount
 
-),
+        from payments
+        group by 1
 
-payments as (
+    ),
 
-    select * from {{ ref('stg_payments') }}
+    final as (
 
-),
+        select
+            orders.order_id,
+            orders.customer_id,
+            orders.order_date,
+            coalesce(order_payments.amount, 0) as amount
 
-order_payments as (
+        from orders
 
-    select
-        order_id,
-        sum(case when payment_status = 'success' then amount end) as amount
+        left join order_payments using (order_id)
 
-    from payments 
-    group by 1
+    )
 
-),
-
-final as (
-
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce(order_payments.amount,0) as amount
-
-    from orders
-
-    left join order_payments using (order_id)
-
-)
-
-select * from final
+select *
+from final
--- models/staging/austin_crime/austin_crime.sql
+++ models/staging/austin_crime/austin_crime.sql
@@ -1,17 +1,9 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='unique_key'
-    )
-}}
+{{ config(materialized="incremental", unique_key="unique_key") }}
 
-with 
-source as (
-    select * from {{ source('austin_crime', 'austin_crime') }}
-)
+with source as (select * from {{ source("austin_crime", "austin_crime") }})
 
-select
-    unique_key as record_id,
-    address,
-    * except(unique_key, address)
+select 
+    unique_key as record_id, 
+    address, 
+    * except (unique_key, address)
 from source
